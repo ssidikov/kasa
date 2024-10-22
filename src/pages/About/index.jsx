e43694fb62b6770f70dc028234394938af4a1154
ssidikov <sardorbek.sidikov7@gmail.com>
2024-10-22 09:33:56 +0200

    Refactor dropdown content in About page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -10,22 +10,25 @@ function About() {
       </div>
       <div className='dropdowns-container'>
         <Dropdown title='Fiabilité'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempor ligula id nisi
-          malesuada, sit amet facilisis neque tincidunt. Integer tincidunt bibendum orci a
-          vulputate.
+          Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes
+          aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.
         </Dropdown>
         <Dropdown title='Respect'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras commodo risus sit amet felis
-          aliquet, at bibendum metus tincidunt. Curabitur dignissim, eros non gravida cursus, ex
-          purus iaculis augue, at posuere velit risus sed lacus.
+          La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement
+          discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre
+          plateforme.
         </Dropdown>
         <Dropdown title='Service'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt, mauris eu
-          facilisis malesuada, velit urna ultricies est, sit amet rutrum lectus odio et velit.
+          La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que
+          chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de
+          respect et de bienveillance.
         </Dropdown>
         <Dropdown title='Sécurité'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pulvinar, libero et
-          elementum malesuada, enim nulla porttitor velit, et dapibus libero ipsum at nisl.
+          La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs,
+          chaque logement correspond aux critères de sécurité établis par nos services. En laissant
+          une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que
+          les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité
+          domestique pour nos hôtes.
         </Dropdown>
       </div>
     </section>
