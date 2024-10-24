421e254ad44778caeb19929790dbc1cdc1d4eaba
ssidikov <sardorbek.sidikov7@gmail.com>
2024-10-24 11:42:03 +0200

    Refactor package.json and index.jsx for gh-pages

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import './styles/styles.sass'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/kasa'>
     <App />
   </BrowserRouter>
 )
